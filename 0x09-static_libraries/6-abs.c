58cfa62f979acd21dbf01058bc1014db0707b80a
el4077 <addiselnatan@gmail.com>
2023-01-10 09:35:46 -0800

    task 1

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes abs val
+ * @t: The number
+ * Return: The absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
+
